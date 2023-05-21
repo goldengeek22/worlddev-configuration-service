82e7deeec9b480ef2b1950364d8876b55ac26682
Alexandre AMEVOR <amevoralexandre@gmail.com>
2023-05-21 06:14:25 +0000

    Set up git as data store for services configuration

---
+++ src/main/java/net/worlddev/configurationservice/ConfigurationServiceApplication.java
@@ -0,0 +1,17 @@
+package net.worlddev.configurationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Alexandre AMEVOR
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+    }
+
+}
